a0b95729916ca5c5818d822d3663109cd3779c48
HafidaHamcherif <58478522+HafidaHamcherif@users.noreply.github.com>
2020-08-06 18:23:38 +0200

    trippy

---
+++ src/Config.js
@@ -0,0 +1,7 @@
+const Config={
+    host:`http://localhost:3001`
+};
+
+console.log('Confing',Config);
+
+export default Config;
---
+++ src/Global.js
@@ -0,0 +1,26 @@
+const Global={
+    color:{
+        primary : '#00a64a',
+        secondary : '#767676',
+        tertiary : '#FFF',
+        disabled : '#CCC',
+    },
+    font:{
+        primary : 'Montserrat',
+    },
+    size:{
+        body : 16,
+        caption : 14,
+        header : 22,
+        navbar : 50,
+    },
+    weight:{
+        body : 400,
+        caption : 200,
+        header : 700,
+    }
+}
+
+console.log('Global',Global);
+
+export default Global;
